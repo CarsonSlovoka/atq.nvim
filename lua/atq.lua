1bb49cee47b7c012b3d1f167d86ab6365c7a2064
Carson <jackparadise520a@gmail.com>
2025-01-12 01:43:36 +0800

    新增command: Atq

--- lua/atq.lua
+++ lua/atq.lua
@@ -15,6 +15,7 @@ function M.add(title, msg, opts)
   -- 加入 at 命令
   local full_command = string.format('echo \'%s\' | at %s', notify_command, at_time)
 
+  print("Executing command: " .. full_command)
   os.execute(full_command)
 end
 
@@ -25,12 +26,18 @@ function M.help()
     string.rep("-", 40),
     "[HELP]",
     string.rep("-", 40),
+    'Atq title body -a 01:08', -- no timeout
+    'Atq title body -a 01:08 -t 3000',
+    'Atq title body -a "01:18 01/12/2025"',
     'echo \'notify-send "Title" "message"\' | at 22:30 01/11/2025',
     'echo \'notify-send -t 3000 "Title" "message"\' | at 22:30 01/11/2025',
     'at 10:00 01/11/2025',
     'at 08:00',
     'at now + 1 hour',
     'at 08:00 tomorrow',
+    '!atq',      -- 查看排程
+    '!at -c 11', -- 查看任務編號為11所要做的內容
+    'atrm 11',   -- 刪除排程
   }
 
   local max_width = 0
@@ -69,5 +76,36 @@ function M.help()
   end, { noremap = true, silent = true, buffer = buf })
 end
 
-return M
+function M.setup()
+  vim.api.nvim_create_user_command("Atq",
+    function(args)
+      if #args.args < 2 then
+        vim.notify("參數至少要2個(tilte, msg)", vim.log.levels.INFO)
+      end
+      local params = vim.split(args.args, " ")
+      local title = params[1]
+      local msg = params[2]
 
+      -- 默認不傳值
+      local timeout = nil
+      local at_time = nil
+
+      -- 可選的 timeout 與 at (例如: `-t 3000`, `-a "22:30"`)
+      for i = 3, #params do
+        if params[i] == "-t" and tonumber(params[i + 1]) then
+          timeout = tonumber(params[i + 1]) -- 設定 timeout 為整數
+        elseif params[i] == "-a" and params[i + 1] then
+          at_time = params[i + 1]
+        end
+      end
+
+      M.add(title, msg, { timeout = timeout, at = at_time })
+    end,
+    {
+      nargs = "*",
+      desc = "新增提醒",
+    }
+  )
+end
+
+return M
